aded5ba38d73c5de64cc8bca7304d7eea0d4a774
Romina Espinosa <romina.espinosa@wizeline.com>
2023-07-12 13:39:26 -0600

    Apply Global CSS

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,11 @@
+/**
+ * @Note
+ * The default export of _app.js is a top-level React component that wraps all the pages
+ * in your application. You can use this component to keep state when navigating between pages,
+ * or to add global styles.
+*/
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
