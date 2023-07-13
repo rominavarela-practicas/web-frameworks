f7710e951b2c0cdbed6e9aed1553fda4c70d2590
Romina Espinosa <romina.espinosa@wizeline.com>
2023-07-12 21:21:30 -0600

    Follow me

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -57,12 +57,11 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/rominavarela"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          Follow me!
         </a>
       </footer>
     </div>
