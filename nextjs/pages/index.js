9835f2b9fda1154b62123a578d47a1e2b7952339
Romina Espinosa <romina.espinosa@wizeline.com>
2023-07-12 21:12:48 -0600

    Fix style

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.scss';
+import styles from '../styles/Index.module.scss';
 
 export default function Home() {
   console.log("Rendering Home");
@@ -27,9 +27,7 @@ export default function Home() {
             </ul>
           </a>
         </div>
-      </main>
-
-      <main>
+      
         <div className={styles.grid}>
           <a href="/ssr/countries" className={styles.card}>
             <h3>List of Countries with Server Side Rendering</h3>
@@ -38,9 +36,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
-
-      <main>
+      
         <div className={styles.grid}>
           <a href="/swr/countries" className={styles.card}>
             <h3>Dynamic Dashboard using Client Side Rendering</h3>
@@ -69,57 +65,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
