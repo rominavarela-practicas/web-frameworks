3496b21383f2c2b11c2b71b513002f46b2b42b55
Romina Espinosa <romina.espinosa@wizeline.com>
2023-07-14 14:53:47 -0600

    Add example in with spa

---
+++ nextjs--auth0-react/components/Nav.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Link from 'next/link';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useRouter } from 'next/router';
+import { Loading } from './Loading';
+
+export function Nav() {
+  const { isAuthenticated, isLoading, user, loginWithRedirect, logout } =
+    useAuth0();
+  const { pathname } = useRouter();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span className="navbar-brand">@auth0/auth0-react</span>
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav">
+          <Link
+            href="/"
+            className={`nav-item nav-link${pathname === '/' ? ' active' : ''}`}
+          >
+            Home
+          </Link>
+          <Link
+            href="/users"
+            className={`nav-item nav-link${
+              pathname === '/users' ? ' active' : ''
+            }`}
+          >
+            Users
+          </Link>
+        </div>
+      </div>
+
+      {isAuthenticated ? (
+        <div>
+          <span id="hello">Hello, {user.name}!</span>{' '}
+          <button
+            className="btn btn-outline-secondary"
+            id="logout"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            logout
+          </button>
+        </div>
+      ) : (
+        <button
+          className="btn btn-outline-success"
+          id="login"
+          onClick={() => loginWithRedirect()}
+        >
+          login
+        </button>
+      )}
+    </nav>
+  );
+}
---
+++ nextjs--auth0-react/pages/_app.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import Router from 'next/router';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Nav } from '../components/Nav';
+import '../components/App.css';
+
+const onRedirectCallback = (appState) => {
+  Router.replace(appState?.returnTo || '/');
+};
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Auth0Provider
+        domain={process.env.NEXT_PUBLIC_DOMAIN}
+        clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+        onRedirectCallback={onRedirectCallback}
+        authorizationParams={{
+          scope: process.env.NEXT_PUBLIC_SCOPE,
+          audience: process.env.NEXT_PUBLIC_AUDIENCE,
+          redirect_uri: typeof window !== 'undefined' && window.location.origin,
+        }}
+      >
+        <Nav />
+        <Component {...pageProps} />
+      </Auth0Provider>
+    );
+  }
+}
+
+export default MyApp;
