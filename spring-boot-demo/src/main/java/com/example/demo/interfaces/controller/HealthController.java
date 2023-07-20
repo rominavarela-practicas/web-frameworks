289c9819a58803374eb0b612593ef607b7ed2396
Romina Espinosa <romina.espinosa@wizeline.com>
2023-07-20 11:47:26 -0600

    Add basic spring boot project

---
+++ spring-boot-demo/src/main/java/com/example/demo/interfaces/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.demo.interfaces.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/health")
+public class HealthController
+{
+    @GetMapping("")
+    public String getHealth()
+    {
+        return "happy";
+    }
+}
