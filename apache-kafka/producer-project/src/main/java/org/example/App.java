7dfd13079fb4bd511d5172c7a331f6a39c49d6d0
Romina Espinosa <Romina.Espinosa.-ND@disney.com>
2023-10-17 12:53:12 -0600

    Send order by kafka producer

--- apache-kafka/producer-project/src/main/java/org/example/App.java
+++ apache-kafka/producer-project/src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+
+        KafkaProducer<String, Integer> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, Integer> record = new ProducerRecord<>("OrderTopic", "Macbook Pro", 10);
+        try {
+            producer.send(record);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            producer.close();
+        }
     }
 }
