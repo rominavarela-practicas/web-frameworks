e5c2cfffb9903e8016202b3ce970874e2c45ae94
Romina Espinosa <romina.espinosa@wizeline.com>
2023-10-18 15:53:26 -0600

    Receive order by consumer

--- apache-kafka/consumer-project/src/main/java/org/example/App.java
+++ apache-kafka/consumer-project/src/main/java/org/example/App.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +17,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.put("group.id", "OrderGroup");
+
+        KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("OrderTopic"));
+        ConsumerRecords<String, Integer> orders = consumer.poll(Duration.ofSeconds(20));
+        for(ConsumerRecord<String, Integer> record: orders) {
+            System.out.println(String.format("Product key = %s, quantity = %s", record.key(), record.value()));
+        }
     }
 }
