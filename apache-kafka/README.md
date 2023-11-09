10313a6a55e223beea6673e0fb43fd4d53e6bf69
Romina Espinosa <romina.espinosa@wizeline.com>
2023-11-09 14:10:30 -0600

    Add additional properties

---
+++ apache-kafka/README.md
@@ -0,0 +1,5 @@
+# Apache Kafka Demo Project
+
+#### Recommendend Resources
+
+Overview of Kafka https://jaceklaskowski.gitbooks.io/apache-kafka/content/kafka-overview.html
--- apache-kafka/producer-project/src/main/java/org/example/App.java
+++ apache-kafka/producer-project/src/main/java/org/example/App.java
@@ -22,6 +22,46 @@ public class App
         props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
         props.put("schema.registry.url", "http://localhost:8085");
 
+        // Additional Properties
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG,
+                // "none"
+                // "snappy" // By Google, Less CPU Consumption
+                "gzip" // Higher Compresion Ratio, Less Network Bandwith
+        );
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG,
+                // The total bytes of memory a Kafka producer can use to buffer records waiting to be sent to the server.
+                // If records are sent faster than they can be delivered to the server the producer will block for MAX_BLOCK_MS_CONFIG
+                // after which it will throw an exception.
+                String.valueOf(32 * 1024 * 1024)
+        );
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG,
+                // Batch size (in bytes)
+                // A Kafka producer will attempt to batch records together into fewer requests whenever multiple records are being sent to the same partition.
+                // Must be at least 0. No attempt will be made to batch records larger than this size.
+                "16384"
+        );
+        props.put(ProducerConfig.LINGER_MS_CONFIG,
+                // The producer will wait for up to the given delay to allow other records to be sent batched together.
+                // Default is 0.
+                "1000"
+        );
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG,
+                // The producer will wait for up to the given delay to allow other records to be sent batched together.
+                // Default is 30 * 1000.
+                "10000"
+        );
+        props.put(ProducerConfig.ACKS_CONFIG,
+                // "all", The leader will wait for the full set of in-sync replicas to acknowledge the record. This guarantees that the record will not be lost.
+                // "0", The producer will not wait for any acknowledgment. No guarantee can be made that the server has received the record.
+                "1" // The leader will write the record to its local log but will respond without awaiting full acknowledgement from all followers.
+        );
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG,
+                // When enabled (true), a producer will ensure that exactly one copy of a message is written to the stream.
+                "false"
+        );
+        props.put(ProducerConfig.RETRIES_CONFIG, "3");
+        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "500");
+
         KafkaProducer<String, TruckCoordinates> producer = new KafkaProducer<>(props);
         TruckCoordinates randomTruck = generateRandomTruck();
         ProducerRecord<String, TruckCoordinates> record = new ProducerRecord<>(TruckConfig.TOPIC_NAME, randomTruck.getId().toString(), randomTruck);
