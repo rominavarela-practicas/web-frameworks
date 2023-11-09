c7d15e36768e544c5a5fd33ae2e9d6efc7bb4d4c
Romina Espinosa <romina.espinosa@wizeline.com>
2023-11-09 16:05:13 -0600

    Add additional properties

--- apache-kafka/consumer-project/src/main/java/org/example/App.java
+++ apache-kafka/consumer-project/src/main/java/org/example/App.java
@@ -29,10 +29,26 @@ public class App
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "OrderGroup");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put("schema.registry.url", "http://localhost:8085");
         props.put("specific.avro.reader", "true");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        // Additional Properties
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                // Behavior when a consumer starts reading a partition without a commited offset
+                "latest" // Ignore previous records
+                // "earliest" // Start from the beggening
+        );
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+        props.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, "123455678");
+        props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG,
+                //
+                "1MB");
+        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "200");
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,
+                // If hearbeat is not sent out for this time window, consumer group will rebalance
+                "10000");
 
         KafkaConsumer<String, TruckCoordinates> consumer = new KafkaConsumer<>(props);
 
